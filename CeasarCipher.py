a73d56a75048a1a30e06ae0a4d716bef56c1f9e5
ImKnownAsG <chris.gieselman@gmail.com>
2025-11-02 00:31:21 -0400

    My suggestions.  I modified it so the ordinal values of the input string are shifted by -32 immediately so they are 0 indexed for the modulo arithmatic applied to the rest of the process.  The mod math makes some of the harder to interpret sections a bit easier to read.

--- CeasarCipher.py
+++ CeasarCipher.py
@@ -1,20 +1,50 @@
-data = input("Enter characters to be encrypted: ")
-print(' ')
-listdata = list(data)
-print(listdata)
-ordlist = []
-for character in range(0, len(listdata)):
-	ordlist.append(ord(listdata[character]))
+def shiftCharacter(char, direct):
+    shiftAmount = 32 #change to 0 if constraints on printable characters are lifted
+    #this will accept a string or integer but only returns an integer
+    if isinstance(char, str):
+        char = ord(char)
+    
+    if direct == 'up':
+        char = char + shiftAmount
+    elif direct == 'down':
+        char = char - shiftAmount
+            
+    return char 
 
-print(f'The individual ordinal values are: {ordlist}')
+modVal = 95 #the number of printable characters, but could be changed if constraints were lifted
+
+while True:
+    ordlist = []
+    printlist = []
+    data = input("Enter characters to be encrypted: ")
+    print(' ')
+    listdata = list(data)
+    print(listdata)
+    
+    for character in range(0, len(listdata)):
+        #shifting all the characters by -32 immediately 
+        #to zero index the values for modulo math
+        char = ord(listdata[character]) #unnecessary after testing
+        shiftChar = shiftCharacter(listdata[character], 'down')
+        #values MUST fall between 31 and 127 (not 31, 127 themselves)
+        #I like this, Caesar would have had to use printable characters
+        
+        if shiftChar < 0 or shiftChar > modVal - 1:
+            print(f'Only printable characters accepted')
+            break
+        else:
+            ordlist.append(shiftChar)
+            printlist.append(char)
+        
+    if len(ordlist) == len(listdata):
+        break
+
+
+print(f'The individual ordinal values are: {printlist}')
+print(f'The ordlist values are: {ordlist}')
 
 length = len(listdata)
 print(f'The length of the input is: {length}\n')
-
-#encryption algorithm
-#values MUST fall between 31 and 127 (not 31, 127 themselves)
-
-#input error detection
 
 while True:
 	try:
@@ -24,46 +54,30 @@ while True:
 	except ValueError:
 		print('Only integers accepted')
 
-while cyphershift > 126:
-	cyphershift = cyphershift - 126
-	print(f'Decreasing shift value to acceptable integer {cyphershift}')
-	
-while cyphershift < -126:
-	cyphershift = cyphershift + 126
-	print(f'Increasing shift value to acceptable integer {cyphershift}')
+cyphershift = cyphershift % modVal
+'''
+in python this will always result in a positive value, which is congruent with
+the negative values we might have arrived at in the original version
+when we encrypt we will again use mod to loop the full list
+'''
 
 #basic encryption concept
 
 encrypt0= []
-benchmark0 = 127 - cyphershift
-print(f'Checking for out of bounds values with {benchmark0}')
-ordindex = 0
 
 for character in range(0, len(ordlist)):
-	if ordlist[ordindex] < benchmark0:
-		encrypt0.append(cyphershift + ordlist[character])
-		#print(f'adding {cyphershift}')
-		print(f'Shifting {ordlist[ordindex]} by {cyphershift}: {encrypt0[ordindex]}\n')
-
-		
-		if encrypt0[character] < 32:
-			encrypt0.append(encrypt0[character] + 95)
-			encrypt0.pop(ordindex)
-			print(f'negative shift detected pushing values below 32 to 126 then continuing shift: {encrypt0[ordindex]}\n')
-
-	else:
-		encrypt0.append(ordlist[character] - benchmark0 + 32)
-		print(f'{ordlist[ordindex]} + {cyphershift} exceeds {benchmark0}')
-		print(f'Subtracting (126 - {cyphershift}) then adding 32')
-	
-	ordindex +=1
+    encryptChar = (ordlist[character] + cyphershift) % modVal
+    encrypt0.append(encryptChar)
 
 print(f'First encrypted values:{encrypt0}')
 
 print(f'Decryption key : {cyphershift}')
 
-decrypt0 = []
+for _ in range(len(encrypt0)):
+    encrypt0[_] = shiftCharacter(encrypt0[_], 'up')
+#we have shifted the values for the cypher, now we have to revert that shift to get to the printable values
 
+decrypt0 = []
 for character in range(0, len(listdata)):
 	decrypt0.append(chr(int((encrypt0[character]))))
 
